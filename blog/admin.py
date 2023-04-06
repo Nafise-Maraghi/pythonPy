cefe0b022fd0f92537dcb1a5e8196e9be4809bd7
Precious <nafiseh.maraghi@gmail.com>
2023-04-06 20:20:10 +0430

    admin.py modified

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'description']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created_at']
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+
+admin.site.register(Category, CategoryAdmin)
